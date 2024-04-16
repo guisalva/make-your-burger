f701244972794652571b4f73d0f3dafafc01b14f
Guilherme Salvador <guilherme@ti18.COBRECOM>
2024-04-16 13:40:02 -0300

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer id="footer">
+        <p>Make Your burger &copy; 2024</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+};
+</script>
+
+<style scoped>
+#footer {
+    height: 200px;
+    width: 100%;
+    background-color: #222;
+    color: #fcba03;
+    border-top: 4px solid #111;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+}
+</style>
